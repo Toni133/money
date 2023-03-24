436d1efe6e5559abbf6526857e09c80f13375a3c
Anton <toniq133@bk.ru>
2023-03-24 13:25:30 +0300

    first commit

--- src/main/java/ru/netology/services/cash.java
+++ src/main/java/ru/netology/services/cash.java
@@ -1,18 +1,18 @@
 package ru.netology.services;
 public class cash {
     public int calculate(int income, int expenses, int threshold) {
-        int count = 0;
+        int counter = 0;
         int maney = 0;
 
         for (int i = 0; i < 12; i++)
-            if (maney <= threshold) {
+            if (maney >= threshold) {
                 maney = maney - expenses;
                 maney = maney / 3;
-                count++;
+                counter++;
             } else {
                 maney = maney + income - expenses;
 
             }
-        return count;
+        return counter;
     }
 }
--- src/test/java/ru/netology/services/cashTest.java
+++ src/test/java/ru/netology/services/cashTest.java
@@ -13,9 +13,16 @@ class cashTest {
         int actual = service.calculate(10_000, 3_000, 20_000);
         int expected = 3;
 
-         Assertions.assertEquals(expected, actual);
-
-
+        Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void test2() {
+        cash service = new cash();
+
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        int expected = 2;
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
